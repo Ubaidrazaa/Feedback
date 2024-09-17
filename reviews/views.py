1bc62eb2e1fe6c61f280b075e79a355b8767242f
Ubaidrazaa <ubaidraza9962@gmail.com>
2024-09-17 16:14:26 -0700

    validation with django form

--- reviews/views.py
+++ reviews/views.py
@@ -4,15 +4,17 @@ from .forms import ReviewForm
 # Create your views here.
 
 def review(request):
-     if request.method == 'POST':
+    if request.method == 'POST':
         form = ReviewForm(request.POST)
      
         if form.is_valid():
           print(form.cleaned_data)
-     
           return HttpResponseRedirect("/thank-you")
+    
+    else:
      form = ReviewForm()
-     return render(request, "reviews/review.html",{
+
+    return render(request, "reviews/review.html",{
         "form" : form
     })
 
