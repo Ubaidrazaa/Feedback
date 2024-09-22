3ef02ad9fb9e50a25ecf7172b7a3c934d5760515
Ubaidrazaa <ubaidraza9962@gmail.com>
2024-09-22 16:02:59 -0700

    Add modelform in forms file and make changes in views

--- reviews/views.py
+++ reviews/views.py
@@ -1,7 +1,6 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from .forms import ReviewForm
-from .models import Review
 # Create your views here.
 
 def review(request):
@@ -9,11 +8,7 @@ def review(request):
         form = ReviewForm(request.POST)
      
         if form.is_valid():
-          review = Review(
-             user_name=form.cleaned_data['user_name'],
-             review_text=form.cleaned_data['review_text'],
-             rating=form.cleaned_data['rating'])
-          review.save()
+          form.save()
           return HttpResponseRedirect("/thank-you")
     
     else:
